13312ada6aa4ef80f6eeb59e1eefe2d185f1d045
KorolokovMaxim <kormaxim93@mail.ru>
2024-01-04 12:09:27 +0400

    init commit

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,6 @@ import parser.BankStatementParser;
 import service.BankStatementAnalyzerService;
 
 public class Main {
-    private final static String RESOURCES = "resources/";
-
     public static void main(String[] args) {
         BankStatementParser csvParser = new BankStatementCSVParser();
         BankStatementAnalyzerService analyzerService = new BankStatementAnalyzerService();
--- src/domain/BankTransaction.java
+++ src/domain/BankTransaction.java
@@ -1,12 +1,7 @@
 package domain;
 
-import notification.Notification;
-
 import java.time.LocalDate;
-import java.time.format.DateTimeParseException;
 import java.util.Objects;
-
-import static java.lang.StringTemplate.STR;
 
 public record BankTransaction(LocalDate date, double amount, String description) {
 
